2c83f229d207ca50c54571ca60064ed556e87ea5
Baptiste B <babu84700@gmail.com>
2021-05-07 13:09:24 +0200

    Create likefunction.js

---
+++ likefunction.js
@@ -0,0 +1,9 @@
+//var compteur=0
+
+function like(){
+    console.log("Start of like function");
+    document.getElementById("fiak").click();
+    compteur=compteur+1;
+    console.log("Like number **** " + compteur +" ****");
+    console.log("end of like function");
+}
